f3fbd11fe75e696ed0a1f3a500ae8044a01e33b9
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-21 15:42:43 -0500

    added a single space

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -35,7 +35,7 @@ def get_completed_tasks(tasks):
 
 def print_employee_tasks(employeeName, completedTasks, totalTasks):
     """prints employee tasks"""
-    print(f"Employee {employeeName} is done with\
+    print(f"Employee {employeeName} is done with \
           tasks({len(completedTasks)}/{totalTasks}):")
     for task in completedTasks:
         task_title = task.get("title")
