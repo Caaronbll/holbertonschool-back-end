737c9962468937bcac86713e0a95f9d2eac63e47
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-21 15:19:20 -0500

    tasks 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress"""
+
+import requests
+import sys
+
+url = "https://jsonplaceholder.typicode.com/"
+
+def get_employee_todos(employee_id):
+    """gets the employye tasks"""
+    url_todos = f"{url}/users/{employee_id}/todos"
+    todos = requests.get(url_todos)
+    return todos.json()
+
+def get_employee_name(employee_id):
+    """gets the employee name"""
+    url_name = f"{url}/users/{employee_id}"
+    employee_name = requests.get(url_name)
+    return employee_name.json().get("name")
+
+def get_completed_tasks(tasks):
+    """gets the completed tasks of the employee"""
+    completed_tasks = []
+    for task in tasks:
+        if task.get("completed"):
+            completed_tasks.append(task)
+    return completed_tasks
+
+def print_employee_tasks(employeeName, completedTasks, totalTasks):
+    """prints employee tasks"""
+    print(f"Employee {employeeName} is done with tasks({len(completedTasks)}/{totalTasks}):")
+    for task in completedTasks:
+        task_title = task.get("title")
+        print(f"\t {task_title}")
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    tasks = get_employee_todos(employee_id)
+    employeeName = get_employee_name(employee_id)
+    completedTasks = get_completed_tasks(tasks)
+    print_employee_tasks(employeeName, completedTasks, len(tasks))
